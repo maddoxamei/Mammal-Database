517197533c4379613438648e79c78e8538753d6b
Mei Maddox <maddoxamei@gmail.com>
2020-04-17 12:33:08 -0400

    Python Foaf

---
+++ app/dev.py
@@ -0,0 +1,4 @@
+from foaf_flask.main import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/foaf_flask/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from rdflib import Graph
+from rdflib.namespace import FOAF
+
+app = Flask(__name__)
+foaf = Graph()
+foaf.parse("flask_foaf/static/Mammal.owl")
+
+
+def get_me():
+    res = foaf.query("""SELECT DISTINCT ?fname
+                     WHERE {
+                     ?me a foaf:Person .
+                     ?me foaf:givenname ?fname .
+                     }
+                     """, initNs={"foaf": FOAF})
+    return list(res)[0]
+
+@app.route("/")
+def home_page():
+    return render_template("index.html",
+                           me= get_me()
+                           )
+
+if __name__ == "__main__":
+    app.run(debug=True)
