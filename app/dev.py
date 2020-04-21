a6f3a7985750d544f3226e039eb58456549e9b3d
maddoxamei <maddoxamei@gmail.com>
2020-04-20 21:14:26 -0400

    Moving Files Around

--- app/dev.py
+++ app/dev.py
@@ -1,4 +1,19 @@
-from foaf_flask.main import app
+from flask import Flask, render_template, url_for, request, jsonify
+from SPARQLWrapper import SPARQLWrapper, JSON
+import requests
+import traceback
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app = Flask(__name__)
+
+#/query, /update, /data HTTP update
+ENDPOINT = 'http://localhost:3030/mammals'
+
+sparql = SPARQLWrapper(ENDPOINT+"/query")
+sparql.setReturnFormat(JSON)
+query = "SELECT * {?subject ?predicate ?object}"
+sparql.setQuery(""" +query+ """)
+
+results = sparql.query().convert()
+
+for result in results["results"]["bindings"]:
+    print(result["label"]["value"])
