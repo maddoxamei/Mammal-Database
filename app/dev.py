91e6f9700d6c6ed66a7837a51ccd295141c88776
maddoxamei <maddoxamei@gmail.com>
2020-04-20 23:17:55 -0400

    Sucessful Query

--- app/dev.py
+++ app/dev.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, jsonify
 from SPARQLWrapper import SPARQLWrapper, JSON
-import requests
-import traceback
+#import requests
+#import traceback
 
 app = Flask(__name__)
 
@@ -10,10 +10,17 @@ ENDPOINT = 'http://localhost:3030/mammals'
 
 sparql = SPARQLWrapper(ENDPOINT+"/query")
 sparql.setReturnFormat(JSON)
-query = "SELECT * {?subject ?predicate ?object}"
-sparql.setQuery(""" +query+ """)
+prefix = """ prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix owl: <http://www.w3.org/2002/07/owl#>
+prefix m: <http://www.semanticweb.org/ontologies/2020/2/Mammal#> """
+query = "SELECT ?class	where { ?class rdfs:subClassOf m:Development }"
+sparql.setQuery(" "" "+prefix+query+" "" ")
 
 results = sparql.query().convert()
 
 for result in results["results"]["bindings"]:
-    print(result["label"]["value"])
+    print(result)
+    #print(result["label"]["value"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
