9c75b51b5f8a9dff3e515d3a0b4c4378a540329a
maddoxamei <maddoxamei@gmail.com>
2020-04-26 14:18:02 -0400

    Adding Records Works

--- app/dev.py
+++ app/dev.py
@@ -10,23 +10,29 @@ app = Flask(__name__)
 g = rdflib.Graph()
 g.parse('static/Mammal.owl', format="application/rdf+xml")
 
-rdf = rdflib.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-rdfs = rdflib.URIRef("http://www.w3.org/2000/01/rdf-schema#")
-owl = rdflib.URIRef("http://www.w3.org/2002/07/owl#")
-xsd = rdflib.URIRef("http://www.w3.org/2001/XMLSchema#")
-xml = rdflib.URIRef("http://www.w3.org/XML/1998/namespace#")
-mammal = rdflib.URIRef("http://www.semanticweb.org/ontologies/2020/2/Mammal#")
+namespaces = {
+    "rdf":rdflib.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+    "rdfs":rdflib.URIRef("http://www.w3.org/2000/01/rdf-schema#"),
+    "owl":rdflib.URIRef("http://www.w3.org/2002/07/owl#"),
+    "xsd":rdflib.URIRef("http://www.w3.org/2001/XMLSchema#"),
+    "xml":rdflib.URIRef("http://www.w3.org/XML/1998/namespace#"),
+    "m":rdflib.URIRef("http://www.semanticweb.org/ontologies/2020/2/Mammal#")
+}
+
 g.bind('rdf', RDF)
 g.bind('rdfs', RDFS)
 g.bind('owl', OWL)
 g.bind('xsd', XSD)
 g.bind('xml', XMLNS)
 g.bind('foaf', FOAF)
-g.bind('m', mammal)
+g.bind('m', namespaces['m'])
 #print(g.serialize().decode("utf-8"))
 print("Total Tripples:\t",len(g))
 
-
+response = {
+    'p':[],
+    'q':[]
+}
 #@app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
@@ -34,23 +40,37 @@ def favicon():
 
 @app.route("/")
 def index():
-    response = ""
     return render_template('layout.html', response=response)
 
 @app.route("/query_page", methods=['POST'])
 def query():
     query = request.form.get('search');
-    response = list(g.query(query))
+    response.update(q=list(g.query(query)))
     return render_template('layout.html', response=response)
 
 @app.route("/add_page", methods=['POST'])
 def add():
-    print (namespace.Development)
-    #g.add((m.Development.test, rdfs.subClassOf, m.Development))
-    query = "SELECT ?class	where { ?class rdfs:subClassOf m:Development }"
-    #response = "After the ADDITION, there are {} triples in the graph".format(len(g))
-    test = rdflib.URIRef(namespace.Development+"/test")
-    response = namespace.Development.test
+    response.update(p = "Incorrect Input Method. A tripple consists of an :")
+
+    sub = request.form.get('sub')
+    pred = request.form.get('pred')
+    obj = request.form.get('obj')
+
+    subject = sub.split(':')
+    predicate = pred.split(':')
+    object = obj.split(':')
+
+    if(len(subject)==2 and len(predicate)==2 and len(object)==2):
+        #subject = rdflib.URIRef(namespaces)
+        subject = rdflib.URIRef(namespaces[subject[0]]+subject[1])
+        predicate = rdflib.URIRef(namespaces[predicate[0]]+predicate[1])
+        object = rdflib.URIRef(namespaces[object[0]]+object[1])
+        g.add((subject, predicate, object))
+        line = ["After the ADDITION, there are {} triples in the graph.".format(len(g)),
+                " To view the recent addition use the following query:",
+                "Select ?"+obj.split(':')[1]+" Where { ?"+obj.split(':')[1]+" "+pred+" "+obj+"}"]
+        response.update(p=line)
+    #response = namespace.Development.test
     return render_template('layout.html', response=response)
 
 @app.route('/<string:page_name>/')
