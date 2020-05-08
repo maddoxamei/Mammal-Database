048ae2b2aac76c2cd72db154a8cf001ec1941708
maddoxamei <maddoxamei@gmail.com>
2020-05-07 22:46:53 -0400

    Remove Unecessary Files

--- Flask/testing/blueprints/user.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-user = Blueprint('user', __name__)
-@user.route('/greeting/<name>/')
-def greeting(name):
-	return render_template('greet.html', name=name)
-	#return 'Hello, lowly normal user!'
